ad89b690d6829e3e4424ce634b4e159a91346e5a
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2023-04-04 22:10:22 -0300

    refactor: Change variables names

--- application_layer/http/http_server.py
+++ application_layer/http/http_server.py
@@ -1,61 +1,64 @@
-from socket import *
-import time
+'''
+João Pedro Melquiades Gomes
+Universidade Federal de Campina Grande
+-------------------------------------------------------------
+Simple server in python to provide a HTTP service to a client
+'''
+import socket
 
 # Host and Port
-HOST = '192.168.100.22'
+HOST = '192.168.0.4'
 PORT = 8000
 
 def main():
-    serverSocket = socket(AF_INET, SOCK_STREAM)
+    '''
+    Main function
+    '''
+    # Creating a TCP connection
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Configuring Socket
-    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    serverSocket.bind((HOST, PORT))
-    serverSocket.listen(1)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
 
     print(f'Listen to port {PORT} ...')
 
     while True:
         print('Waiting for client to connect...')
-        
-        connectionSocket, addr = serverSocket.accept()
+        connection_socket, addr = server_socket.accept()
 
         print(f'Client {addr[0]} connected from port {addr[1]}')
 
         try:
-            message = connectionSocket.recv(1024).decode()
-            
+            message = connection_socket.recv(1024).decode()
             filename = message.split()[1]
             print(f'Client requested the following file: {filename}')
 
             f = open(filename[1:], 'rb')
 
-            outputdata = f.readlines()
-            
+            resp_data = f.readlines()
             # Sending header
-            respHeader = 'HTTP/1.1 200 OK\n\n'
-            connectionSocket.send(respHeader.encode())
+            resp_header = 'HTTP/1.1 200 OK\n\n'
+            connection_socket.send(resp_header.encode())
 
             # sending response
-            for i, line in enumerate(outputdata, len(outputdata)):
-                connectionSocket.send(line)
-            
-            connectionSocket.send('\r\n'.encode())
+            for line in resp_data:
+                connection_socket.send(line)
+            connection_socket.send('\r\n'.encode())
 
         except IOError:
-            respHeader = 'HTTP/1.1 404 Not Found\n\n'
-            connectionSocket.send(respHeader.encode())
+            resp_header = 'HTTP/1.1 404 Not Found\n\n'
+            connection_socket.send(resp_header.encode())
 
             file404 = open('404_not_found.html', 'rb')
-            
-            respData = file404.readlines()
+            resp_data = file404.readlines()
 
-            for line in respData:
-                connectionSocket.send(line)
-            
-            connectionSocket.send('\r\n'.encode())
+            for line in resp_data:
+                connection_socket.send(line)
+            connection_socket.send('\r\n'.encode())
 
-        connectionSocket.close()
+        connection_socket.close()
 
 if __name__ == '__main__':
     main()
