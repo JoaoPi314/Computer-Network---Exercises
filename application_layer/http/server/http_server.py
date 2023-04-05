53e44eea14cbfd897a5a844ea64c94f7f6e9f939
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2023-04-04 23:48:40 -0300

    refactor: Remove trash code

--- application_layer/http/server/http_server.py
+++ application_layer/http/server/http_server.py
@@ -71,44 +71,7 @@ def main():
     '''
 
     print(f'Listen to port {PORT} ...')
-
     main_thread()
-
-    # while True:
-    #     print('Waiting for client to connect...')
-    #     connection_socket, addr = server_socket.accept()
-
-    #     print(f'Client {addr[0]} connected from port {addr[1]}')
-
-    #     try:
-    #         message = connection_socket.recv(1024).decode()
-    #         filename = message.split()[1]
-    #         print(f'Client requested the following file: {filename}')
-
-    #         f = open(filename[1:], 'rb')
-
-    #         resp_data = f.readlines()
-    #         # Sending header
-    #         resp_header = 'HTTP/1.1 200 OK\n\n'
-    #         connection_socket.send(resp_header.encode())
-
-    #         # sending response
-    #         for line in resp_data:
-    #             connection_socket.send(line)
-    #         connection_socket.send('\r\n'.encode())
-
-    #     except IOError:
-    #         resp_header = 'HTTP/1.1 404 Not Found\n\n'
-    #         connection_socket.send(resp_header.encode())
-
-    #         file404 = open('404_not_found.html', 'rb')
-    #         resp_data = file404.readlines()
-
-    #         for line in resp_data:
-    #             connection_socket.send(line)
-    #         connection_socket.send('\r\n'.encode())
-
-    #     connection_socket.close()
 
 if __name__ == '__main__':
     main()
